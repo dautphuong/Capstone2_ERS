7a789ba8f5b9bc6570af3caa71b51dec7de2bdf7
TechCare <linhle8599@gmail.com>
2021-03-20 16:46:55 +0700

    create register

---
+++ app/ERSystem/Components/Regiter.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    TextInput,
+    Keyboard,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+} from 'react-native';
+
+export default class Resgiter extends Component {
+    render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
