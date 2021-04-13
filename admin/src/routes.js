d57af96a5b0d3e790344e4910409b033a525f92e
Toan Truong <toan.truong@sapotacorp.vn>
2021-04-13 17:01:51 +0700

    add admin

--- admin/src/routes.js
+++ admin/src/routes.js
@@ -18,21 +18,21 @@
 import Dashboard from "views/Dashboard.js";
 import UserProfile from "views/UserProfile.js";
 import TableList from "views/TableList.js";
-import Typography from "views/Typography.js";
+import Lesson from "views/Lesson";
 import Icons from "views/Icons.js";
 import Maps from "views/Maps.js";
 import Notifications from "views/Notifications.js";
 import Upgrade from "views/Upgrade.js";
 
 const dashboardRoutes = [
-  {
-    upgrade: true,
-    path: "/upgrade",
-    name: "Upgrade to PRO",
-    icon: "nc-icon nc-alien-33",
-    component: Upgrade,
-    layout: "/admin",
-  },
+  // {
+  //   upgrade: true,
+  //   path: "/upgrade",
+  //   name: "Upgrade to PRO",
+  //   icon: "nc-icon nc-alien-33",
+  //   component: Upgrade,
+  //   layout: "/admin",
+  // },
   {
     path: "/dashboard",
     name: "Dashboard",
@@ -40,37 +40,37 @@ const dashboardRoutes = [
     component: Dashboard,
     layout: "/admin",
   },
-  {
-    path: "/user",
-    name: "User Profile",
-    icon: "nc-icon nc-circle-09",
-    component: UserProfile,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/user",
+  //   name: "Manager User",
+  //   icon: "nc-icon nc-notes",
+  //   component: UserProfile,
+  //   layout: "/admin",
+  // },
   {
     path: "/table",
-    name: "Table List",
-    icon: "nc-icon nc-notes",
+    name: "Manager User",
+    icon: "nc-icon nc-circle-09",
     component: TableList,
     layout: "/admin",
   },
   {
-    path: "/typography",
-    name: "Typography",
+    path: "/lesson",
+    name: "Lesson",
     icon: "nc-icon nc-paper-2",
-    component: Typography,
+    component: Lesson,
     layout: "/admin",
   },
   {
     path: "/icons",
-    name: "Icons",
+    name: "Contest",
     icon: "nc-icon nc-atom",
     component: Icons,
     layout: "/admin",
   },
   {
     path: "/maps",
-    name: "Maps",
+    name: "Exam",
     icon: "nc-icon nc-pin-3",
     component: Maps,
     layout: "/admin",
