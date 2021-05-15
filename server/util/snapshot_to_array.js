95d4ba52beb3408a252e43d90d4f0f0f69e61978
Toan Truong <toan.truong@sapotacorp.vn>
2021-05-16 00:01:41 +0700

    add Client

--- server/util/snapshot_to_array.js
+++ server/util/snapshot_to_array.js
@@ -1,10 +1,9 @@
 const firebase = require("./firebase_connect");
 
-var returnArr2=[];
-
 module.exports = {
     snap_array: function(snapshot) {
-        var returnArr=[];
+        var returnArr = [];
+
         snapshot.forEach(function(childSnapshot) {
             var item = childSnapshot.val();
             item.id = childSnapshot.key;
@@ -12,26 +11,5 @@ module.exports = {
         });
 
         return returnArr;
-    },
-    
-    snap_arr:  function(snapshot) {
-
-         snapshot.forEach( function(childSnapshot) {
-            firebase.database().ref("topics/" + childSnapshot.val().idTopic).once("value").then(function(snapshot) {
-                if (snapshot.exists()) {
-                    var item = childSnapshot.val();
-                    item.id = childSnapshot.key;
-                    getNameTopic(item,snapshot.val().name)
-                } 
-            }); 
-        });
-        
-        return returnArr2;
-    },
-}
-
-var getNameTopic = (item,nameTopic) => {
-    item.nameTopic=nameTopic;
-    returnArr2.push(item);
-    console.log(returnArr2[0].id)
+    }
 }
